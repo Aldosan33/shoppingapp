427ec85e66637dc3ee14f98d7c5b44fe8769ad68
= <alfredo.2993@gmail.com>
2024-03-31 17:38:08 -0500

    Setting up environment

---
+++ Shopping/Shopping.Client/Data/ProductContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Shopping.Client.Models;
+
+namespace Shopping.Client.Data
+{
+    public static class ProductContext
+    {
+        public static readonly List<Product> Products = new List<Product>
+        {
+            new Product()
+            {
+                Name = "IPhone X",
+                Description = "This phone is the 10th edition of iphone models",
+                ImageFile = "product-1.png",
+                Price = 950.00M,
+                Category  = "IPhone"
+            },
+            new Product()
+            {
+                Name = "Samsung 10",
+                Description = "This phone is the 10th edition of samsung models",
+                ImageFile = "product-2.png",
+                Price = 840.00M,
+                Category  = "Smart Phone"
+            },
+        };
+    }
+}
